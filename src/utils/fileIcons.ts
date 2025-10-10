d33dce17612736fccb889eb86e3691dda932ec23
FriquetLuca <friquet.luca@gmail.com>
2025-10-10 20:10:14 +0200

    optimize the file icons search

---
+++ src/utils/fileIcons.ts
@@ -0,0 +1,267 @@
+interface FileIcon {
+  src: string;
+  alt: string;
+}
+
+export const fileIcons: Record<string, FileIcon | undefined> = {
+  md: {
+    src: '/markdown.svg',
+    alt: 'markdown',
+  },
+  js: {
+    src: '/javascript.svg',
+    alt: 'javascript',
+  },
+  mjs: {
+    src: '/javascript.svg',
+    alt: 'javascript',
+  },
+  jsx: {
+    src: '/jsx.svg',
+    alt: 'javascript',
+  },
+  json: {
+    src: '/json.svg',
+    alt: 'json',
+  },
+  ts: {
+    src: '/typescript.svg',
+    alt: 'typescript',
+  },
+  mts: {
+    src: '/typescript.svg',
+    alt: 'typescript',
+  },
+  tsx: {
+    src: '/tsx.svg',
+    alt: 'typescript',
+  },
+  html: {
+    src: '/html.svg',
+    alt: 'html',
+  },
+  css: {
+    src: '/css.svg',
+    alt: 'css',
+  },
+  rs: {
+    src: '/rust.svg',
+    alt: 'rust',
+  },
+  toml: {
+    src: '/toml.svg',
+    alt: 'toml',
+  },
+  sass: {
+    src: '/sass.svg',
+    alt: 'sass',
+  },
+  scss: {
+    src: '/sass.svg',
+    alt: 'sass',
+  },
+  svg: {
+    src: '/svg.svg',
+    alt: 'svg',
+  },
+  yml: {
+    src: '/yaml.svg',
+    alt: 'yaml',
+  },
+  yaml: {
+    src: '/yaml.svg',
+    alt: 'yaml',
+  },
+  sh: {
+    src: '/shell.svg',
+    alt: 'shell',
+  },
+  ps1: {
+    src: '/powershell.svg',
+    alt: 'powershell',
+  },
+  cpp: {
+    src: '/c-plus-plus.svg',
+    alt: 'C++',
+  },
+  cc: {
+    src: '/c-plus-plus.svg',
+    alt: 'C++',
+  },
+  cxx: {
+    src: '/c-plus-plus.svg',
+    alt: 'C++',
+  },
+  'c++': {
+    src: '/c-plus-plus.svg',
+    alt: 'C++',
+  },
+  py: {
+    src: '/python.svg',
+    alt: 'python',
+  },
+  pyc: {
+    src: '/python.svg',
+    alt: 'python',
+  },
+  pyd: {
+    src: '/python.svg',
+    alt: 'python',
+  },
+  pyo: {
+    src: '/python.svg',
+    alt: 'python',
+  },
+  tex: {
+    src: '/tex.svg',
+    alt: 'tex',
+  },
+  cs: {
+    src: '/c-sharp.svg',
+    alt: 'C#',
+  },
+  c: {
+    src: '/c.svg',
+    alt: 'C',
+  },
+  java: {
+    src: '/java.svg',
+    alt: 'java',
+  },
+  class: {
+    src: '/java.svg',
+    alt: 'java',
+  },
+  jar: {
+    src: '/java.svg',
+    alt: 'java',
+  },
+  jad: {
+    src: '/java.svg',
+    alt: 'java',
+  },
+  jmod: {
+    src: '/java.svg',
+    alt: 'java',
+  },
+  go: {
+    src: '/go.svg',
+    alt: 'go',
+  },
+  xml: {
+    src: '/xml.svg',
+    alt: 'xml',
+  },
+  cbl: {
+    src: '/cobol.svg',
+    alt: 'cobol',
+  },
+  cob: {
+    src: '/cobol.svg',
+    alt: 'cobol',
+  },
+  cpy: {
+    src: '/cobol.svg',
+    alt: 'cobol',
+  },
+  kt: {
+    src: '/kotlin.svg',
+    alt: 'kotlin',
+  },
+  kts: {
+    src: '/kotlin.svg',
+    alt: 'kotlin',
+  },
+  scala: {
+    src: '/scala.svg',
+    alt: 'scala',
+  },
+  sc: {
+    src: '/scala.svg',
+    alt: 'scala',
+  },
+  h: {
+    src: '/header.svg',
+    alt: 'header',
+  },
+  hpp: {
+    src: '/header.svg',
+    alt: 'header',
+  },
+  hh: {
+    src: '/header.svg',
+    alt: 'header',
+  },
+  hxx: {
+    src: '/header.svg',
+    alt: 'header',
+  },
+  'h++': {
+    src: '/header.svg',
+    alt: 'header',
+  },
+  php: {
+    src: '/php.svg',
+    alt: 'php',
+  },
+  phps: {
+    src: '/php.svg',
+    alt: 'php',
+  },
+  phar: {
+    src: '/php.svg',
+    alt: 'php',
+  },
+  phtml: {
+    src: '/php.svg',
+    alt: 'php',
+  },
+  pht: {
+    src: '/php.svg',
+    alt: 'php',
+  },
+  lua: {
+    src: '/lua.svg',
+    alt: 'lua',
+  },
+  luna: {
+    src: '/lua.svg',
+    alt: 'lua',
+  },
+  lunaire: {
+    src: '/lua.svg',
+    alt: 'lua',
+  },
+  anair: {
+    src: '/lua.svg',
+    alt: 'lua',
+  },
+  hl: {
+    src: '/haskell.svg',
+    alt: 'haskell',
+  },
+  lhs: {
+    src: '/haskell.svg',
+    alt: 'haskell',
+  },
+  exe: {
+    src: '/file-exe.svg',
+    alt: 'executable',
+  },
+  dll: {
+    src: '/file-exe.svg',
+    alt: 'executable',
+  },
+  ttf: {
+    src: '/font.svg',
+    alt: 'font',
+  },
+  woff: {
+    src: '/font.svg',
+    alt: 'font',
+  },
+  woff2: {
+    src: '/font.svg',
+    alt: 'font',
+  },
+};
